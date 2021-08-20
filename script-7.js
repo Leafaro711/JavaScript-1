db5895a99ae602dfc7a44820740b61153d6c153d
Leafaro711 <leafaro@mail.ru>
2021-08-20 10:38:23 +0400

    finished

--- script-7.js
+++ script-7.js
@@ -1,1 +1,14 @@
 // 7) Функция, которая принимает число до 1000 и возвращает массив из делителей этого числа
+
+let userNumber = prompt("Введите число от 1 до 1000");
+function devisers(userNumber) {
+    let array = [];
+    for(i = 1; i < 1000; i++) {
+        if(userNumber % i === 0) {
+            array[i - 1] = i;
+            alert(array);
+        }
+    }
+}
+
+devisers(userNumber);
